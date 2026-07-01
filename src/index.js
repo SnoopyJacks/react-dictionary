a4e9da9cb44f09474332d837d15972933cb9834f
Justine <t.nichole.2807@gmail.com>
2026-07-01 08:00:45 -0600

    debugged the index.js

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import React { StrictMode} from 'react';
+import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import './index.css';
 import App from './App';
@@ -6,8 +6,8 @@ import App from './App';
 const rootElement = document.getElementById('root');
 const root = createRoot(rootElement);
 root.render(
-  <React.StrictMode>
+  <StrictMode>
     <App />
-  </React.StrictMode>
+  </StrictMode>
 );
 
