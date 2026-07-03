62567889de911757c2fd8626fa47616a545e4ac0
Justine <t.nichole.2807@gmail.com>
2026-07-03 09:20:13 -0600

    updated css

--- src/App.js
+++ src/App.js
@@ -6,16 +6,32 @@ export default function App() {
   return (
     <div className="App">
       <div className="container">
-      <header className="App-header">
-        <img src={logo} className="App-logo img-fluid" alt="logo" />
-      </header>
-      <main>
-        <Dictionary defaultKeyword="computer" />
-      </main>
-      <footer className="text-center">
-        Coded by Justine Ziola
-      </footer>
-    </div>
+        <header className="App-header">
+          <img src={logo} className="App-logo img-fluid" alt="logo" />
+        </header>
+        <main>
+          <Dictionary defaultKeyword="computer" />
+        </main>
+        <footer className="text-center">
+          Coded by Justine Ziola · Open-source on{" "}
+          <a
+            href="https://github.com/SnoopyJacks/react-dictionary"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            GitHub
+          </a>{" "}
+          · Hosted on{" "}
+          <a
+            href="https://reactappdictionary.netlify.app"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Netlify
+          </a>
+          .
+        </footer>
+      </div>
     </div>
   );
 }
